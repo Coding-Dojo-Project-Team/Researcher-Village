17ebfbe734e95a7a2db5c02666bf6b003943ad7c
aacole9 <110492564+aacole9@users.noreply.github.com>
2023-01-28 19:03:11 -0500

    Project stuff

--- flask_app/controllers/projects.py
+++ flask_app/controllers/projects.py
@@ -5,3 +5,58 @@ from flask import flash, redirect, render_template, request, session
 from flask_app import app
 from flask_app.models.user import User
 from flask_app.models.project import Project
+
+@app.route("/post")
+def post_blog():
+    return render_template("post.html")
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("dashboard.html", user=User.get_by_id(data))
+
+@app.route('/project')
+def projects():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("project.html",user=User.get_by_id(data), new_system = Project.get_all_projects())
+
+@app.route('/projects/to_db', methods=["POST"])      
+def project():
+    if 'user_id' not in session:
+        return redirect("/")
+    if not Project.validate_project(request.form):
+        return redirect("/post")
+    data = {
+        "project_name": request.form['project_name'],
+        "category": request.form['category'],
+        "user_id":session["user_id"]
+    }
+    Project.project_insert(data)  
+    return redirect("/project")
+
+@app.route("/project/delete/<int:id>")
+def delete_project(id):
+    if "user_id" not in session:
+        return redirect("/")
+    data = {
+        "id" : id
+    }
+    Project.delete_project(data)
+    return redirect("/project")
+
+@app.route("/project/edit/<int:id>")
+def edit_project (id):
+    if 'user_id' not in session:
+        return redirect("/")
+    data = {
+        "id" : id,  
+    }
+    return render_template("update.html", user=User.get_by_id(data), "Place Holder" = Project.get_one_project(data))
--- flask_app/models/project.py
+++ flask_app/models/project.py
@@ -14,7 +14,7 @@ class Project:
 
     @classmethod
     def project_insert(cls,data):
-        query = "INSERT INTO projects (project_name, category, user_id) VALUES(%(project_name)s,%(cetgory)s, %(user_id)s);"
+        query = "INSERT INTO projects (project_name, category, user_id) VALUES(%(project_name)s,%(category)s, %(user_id)s);"
         return connectToMySQL(cls.db).query_db(query,data)
     
     @classmethod
@@ -35,7 +35,7 @@ class Project:
                     "first_name" : project_dictionary['first_name'],
                     "last_name" : project_dictionary['last_name'],
                     "email" : project_dictionary['email'],
-                    "class"
+                    "year" : project_dictionary ['year'],
                     "password" : project_dictionary['password'],
                     "created_at" : project_dictionary['users.created_at'],
                     "updated_at" : project_dictionary['users.updated_at'],
@@ -47,3 +47,46 @@ class Project:
 
                 all_projects.append(project_obj)
             return all_projects
+        
+    @classmethod
+    def get_one_project(cls,data):
+        query = "SELECT * FROM project JOIN users ON project.user_id = users.id WHERE project.id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        print(results)
+        
+        if len(results) == 0:    
+            return None
+        else: 
+            project_dictionary = results[0]
+            project_obj = cls(results[0])
+            user_dictionary = {
+                    "id" : project_dictionary['users.id'],
+                    "first_name" : project_dictionary['first_name'],
+                    "last_name" : project_dictionary['last_name'],
+                    "email" : project_dictionary['email'],
+                    "year" : project_dictionary ['year'],
+                    "password" : project_dictionary['password'],
+                    "created_at" : project_dictionary['users.created_at'],
+                    "updated_at" : project_dictionary['users.updated_at'],
+                }
+
+            project_obj = User(user_dictionary)
+
+            project_obj.user = user_obj
+            return project_obj
+        
+    @staticmethod
+    def validate_project(form_data):
+        is_valid = True
+        print(form_data)
+        return is_valid   # Please let me know desired validations so I can enter them
+    
+    @classmethod
+    def delete_project(cls, data):
+        query = "DELETE FROM project WHERE id =%(id)s"
+        return connectToMySQL(cls.db).query_db(query,data)
+    
+    @classmethod
+    def project_edit(cls, data):
+        query = "UPDATE project SET project_name = %(project_name)s, category = %(category)s WHERE id =%(id)s"
+        return connectToMySQL(cls.db).query_db(query,data)
